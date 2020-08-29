7601889714d55e33b6c2c95f0e1b82ea3d9ab96f
lnalborczyk <ladislas.nalborczyk@gmail.com>
2020-08-29 23:47:25 +0200

    solves some conflicts bw parameters

--- app.R
+++ app.R
@@ -30,7 +30,11 @@ ui <- shinyUI(
             fluidPage(
                 
                 fluidRow(h3("Simulating response time distributions") ),
-                fluidRow(h4("The code underlying this model can be found at https://github.com/lnalborczyk/shiny_DDM.") ),
+                fluidRow(h4(HTML(
+                "The code underlying this application can be found at
+                <a href='https://github.com/lnalborczyk/shiny_DDM'>
+                https://github.com/lnalborczyk/shiny_DDM</a>."
+                ) ) ),
                 
                 fluidRow(
                     sidebarLayout(
@@ -68,8 +72,8 @@ ui <- shinyUI(
                                 inputId = "delta",
                                 label = "Select a value for the drift rate",
                                 value = 0,
-                                min = -5,
-                                max = 5,
+                                min = -2,
+                                max = 2,
                                 step = 0.1,
                                 width = "500px"
                                 ),
@@ -125,11 +129,10 @@ server <- function (input, output) {
         renderPlot({
             input$refresh
             
-            # nobs = 1e2; alpha = 2; beta = 0.5; delta = 0.5; tau = 0.3;
+            # nobs = 1e3; alpha = 2; beta = 0.5; delta = 0.5; tau = 0.3;
             # df <- rwiener(n = nobs, alpha = alpha, tau = tau, beta = beta, delta = delta)
             
                 df <- rwiener(
-                    # n = as.numeric(input$nobs),
                     n = 1e3,
                     alpha = as.numeric(input$alpha),
                     beta = as.numeric(input$beta),
@@ -161,7 +164,7 @@ server <- function (input, output) {
                 upper_limit <- 0 + s / 2
                 beta_s <- lower_limit + beta * s
                 
-                # computes angle of drift rate
+                # computes the implied angle of the drift rate
                 drift_angle <- atan(delta) * 180 / pi
                 
                 # new data frame
@@ -170,19 +173,22 @@ server <- function (input, output) {
                     resp = rep(c("upper", "lower"), each =  length(ud$x) + 2)
                     )
                 
+                # gets maximum x-axis value
+                max_x <- max(df2$x)
+                
                 df2 %>%
                     ggplot(aes(x = x, y = y, fill = resp, color = resp) ) +
                     # plotting densities
                     geom_segment(
                         data = . %>% filter(resp == "upper"),
-                        aes(x = 0, xend = max(x), y = min(y), yend = min(y) ),
+                        aes(x = 0, xend = max_x, y = min(y), yend = min(y) ),
                         color = "orangered"
-                    ) +
+                        ) +
                     geom_segment(
                         data = . %>% filter(resp == "lower"),
-                        aes(x = 0, xend = max(x), y = max(y), yend = max(y) ),
+                        aes(x = 0, xend = max_x, y = max(y), yend = max(y) ),
                         color = "steelblue"
-                    ) +
+                        ) +
                     geom_polygon(alpha = 0.8) +
                     scale_fill_manual(
                         values = c("steelblue", "orangered"),
@@ -202,7 +208,7 @@ server <- function (input, output) {
                         size = 4, angle = 90,
                         label = "stimulus onset",
                         color = "grey30"
-                    ) +
+                        ) +
                     # starting point
                     geom_hline(yintercept = beta_s, lty = 3, col = "purple") +
                     annotate(
@@ -213,46 +219,44 @@ server <- function (input, output) {
                         size = 4,
                         label = "starting point",
                         colour = "purple"
-                    ) +
+                        ) +
                     # non-decision time
                     annotate(
                         geom = "rect",
                         xmin = 0, xmax = tau,
                         ymin = -Inf, ymax = Inf,
                         alpha = 0.3
-                    ) +
-                    # annotate(
-                    #     geom = "label",
-                    #     x = 0, y = 0,
-                    #     hjust = 0.01, vjust = ifelse(delta < 0, -1, 1.5),
-                    #     size = 4,
-                    #     label = "non-decision time"
-                    # ) +
-                    # drift rate
-                    geom_segment(
-                        aes(
-                            x = tau,
-                            # xend = tau + 0.1,
-                            xend = tau + 0.1 * max(df2$x),
-                            y = beta_s,
-                            # yend = log(delta / (1 - beta) ) + delta / 2
-                            yend = beta_s + delta
-                            ),
-                        arrow = arrow(length = unit(0.2, "cm"), ends = "last", type = "closed"),
-                        size = 0.5, colour = "darkgreen"
                         ) +
-                    # geom_spoke(
-                    #     # aes(x = tau, y = beta_s, angle = 90, radius = 1),
-                    #     aes(x = tau, y = beta_s, angle = drift_angle, radius = 1),
-                    #     arrow = arrow(length = unit(0.2, "cm"), ends = "last", type = "closed"),
-                    #     color  = "green"
+                    # drift rate
+                    # geom_segment(
+                    #     aes(
+                    #         x = tau,
+                    #         # xend = tau + 0.1,
+                    #         xend = tau + 0.1 * max_x,
+                    #         y = beta_s,
+                    #         # yend = log(delta / (1 - beta) ) + delta / 2
+                    #         # yend = beta_s + delta
+                    #         yend = ifelse(beta_s + delta > s, s, beta_s + delta)
+                    #         ),
+                    #     arrow = arrow(
+                    #         length = unit(0.2, "cm"),
+                    #         ends = "last", type = "closed"
+                    #         ),
+                    #     size = 0.5, colour = "darkgreen"
                     #     ) +
+                    geom_spoke(
+                        aes(x = tau, y = beta_s, angle = drift_angle * (pi / 180), radius = 0.5),
+                        arrow = arrow(length = unit(0.2, "cm"), ends = "last", type = "closed"),
+                        color  = "darkgreen"
+                        ) +
                     annotate(
                         geom = "label",
-                        x = tau + 0.1,
+                        # x = tau + 0.1,
+                        x = tau,
                         # y =  log(beta / (1 - beta) ) + delta / 2,
-                        y = 0.1^2 + delta,
-                        hjust = -0.05, vjust = 0,
+                        # y = 0.1^2 + delta,
+                        y = 0,
+                        hjust = 1, vjust = 0.5,
                         size = 4,
                         label = "drift rate",
                         color = "darkgreen"
@@ -262,7 +266,7 @@ server <- function (input, output) {
                         aes(
                             x = mean(x), xend = mean(x),
                             y = -s, yend = s,
-                        ),
+                            ),
                         arrow = arrow(
                             length = unit(0.2, "cm"),
                             ends = "both", type = "closed"
@@ -297,13 +301,9 @@ server <- function (input, output) {
                     theme(
                         axis.text.y = element_blank(),
                         plot.margin = unit(c(1, 1, 1, 3), "cm")
-                    ) +
+                        ) +
                     labs(x = "Reaction time (in seconds)", y = "") +
-                    coord_cartesian(
-                        xlim = c(0, NA),
-                        # ylim = c(-2, 2),
-                        clip = "off"
-                        )
+                    coord_cartesian(xlim = c(0, NA), clip = "off")
                 
             })
     
